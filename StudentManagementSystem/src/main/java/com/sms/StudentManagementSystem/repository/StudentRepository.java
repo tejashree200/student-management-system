93979f337ca23f14d225fccb6d2e0ae28520819e
tejashree200 <96370814+tejashree200@users.noreply.github.com>
2023-10-22 17:42:44 +0530

    Delete StudentManagementSystem directory

--- StudentManagementSystem/src/main/java/com/sms/StudentManagementSystem/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sms.StudentManagementSystem.repository;
-
-import com.sms.StudentManagementSystem.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
